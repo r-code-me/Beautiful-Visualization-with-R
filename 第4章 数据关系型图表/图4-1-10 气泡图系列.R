4c7e7532acab7450485f2480faed410939810ccd
agileabc <agileabc@sina.com>
2022-05-02 16:22:02 +0800

    Working solution.

--- 第4章 数据关系型图表/图4-1-10 气泡图系列.R
+++ 第4章 数据关系型图表/图4-1-10 气泡图系列.R
@@ -45,14 +45,19 @@ ggplot(mtcars, aes(wt,mpg))+
 # 图例的要水平摆放，且放进图片中。
 
 ggplot(data=mtcars, aes(x=wt,y=mpg))+
-  geom_point(aes(size=disp,fill=disp),shape=21,colour="black",alpha=0.8)+
+  geom_point(aes(size=disp,fill=disp),shape=21,colour="white",alpha=0.8)+
   
-  geom_smooth(method="lm",se=TRUE,formula=y ~ log(x),colour="red")+
+  geom_smooth(method="lm", se=TRUE, formula=y ~ log(x), colour="#728893", linetype = "dashed")+
   
-  scale_fill_gradient2(low="#E7C68C", high="#1F4F70", midpoint = mean(mtcars$disp))+ # 汽泡填充颜色的范围
+  scale_fill_gradient2(low="#E7C68C", mid="#AED0BD", high="#1F4F70", midpoint = mean(mtcars$disp))+ # 汽泡填充颜色的范围
 
-  
-  geom_text_repel(label = disp)+ # 汽泡文字
+  geom_text_repel(
+    label = disp, 
+    fontface = 'plain', 
+    color = '#626160', 
+    # box.padding = unit(0.5, "lines"),
+    point.padding = unit(6, "lines"),
+    size = 3)+ # 汽泡文字
   scale_size_area(max_size=8)+ # 汽泡面积最大值
   scale_y_continuous(position = "right")+ # Y轴的刻度放置在右边
   guides(size = guide_legend((title="Value")), fill = guide_legend((title="Value")))+ # 图例
@@ -60,11 +65,11 @@ ggplot(data=mtcars, aes(x=wt,y=mpg))+
     legend.text = element_text(size=10, face="plain", color="black"),
     axis.title = element_text(size=10, face="plain", color="black"),
     axis.text = element_text(size=10, face="plain", color="black"),
-    axis.line = element_line(size = 0.1, linetype = "solid", colour = "#CCCCCD"),
+    axis.line = element_line(size = 0.5, linetype = "solid", colour = "#CCCCCD"),
     panel.background = element_rect(fill = "white"),
     legend.position = "right",
     panel.grid.major = element_line(colour = "#CCCCCD"),
-    panel.grid.minor = element_line(colour = "#CCCCCD")
+    panel.grid.minor = element_line(colour = "white")
   )
 
 
